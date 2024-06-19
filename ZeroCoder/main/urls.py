e574a4c336531132ad2e4ca310d25ffa58f42552
elizarov09 <47592275+elizarov09@users.noreply.github.com>
2024-06-19 23:21:20 +0300

    dj02

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -1,10 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
 
-from django.urls import path, include
-from . import views
 urlpatterns = [
-
-    path("", views.index),
-    path('new', views.new),
-    path('data', views.data),
-    path('test', views.test)
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('data/', views.data, name='data'),
+    path('test/', views.test, name='test'),
+    path('blog/', views.blog, name='blog'),  # Добавляем маршрут для страницы блога
+    path('contacts/', views.contacts, name='contacts'),  # Добавляем маршрут для страницы контактов
+    path('about/', views.about, name='about'),  # Добавляем маршрут для страницы о нас
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello DJANGO</h1>")
+    return render(request, 'main/index.html')
 
 def new(request):
-    return HttpResponse("<h1>Hello DJANGO - 2</h1>")
+    return render(request, 'main/new.html')
 
 def data(request):
     return HttpResponse("<h1>Это DJANGO DATA</h1>")
 
 def test(request):
     return HttpResponse("<h1>Это DJANGO TEST</h1>")
+
+def blog(request):
+    return render(request, 'main/blog.html')  # Представление для страницы блога
+
+def contacts(request):
+    return render(request, 'main/contacts.html')  # Представление для страницы контактов
+
+def about(request):
+    return render(request, 'main/about.html')  # Представление для страницы о нас
